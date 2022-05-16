efcccd479aa4c26126a78323be761d12206263cb
hiroki <hiroki12malu@gmail.com>
2022-05-16 23:14:34 +0900

    保存したテキストを閲覧する

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -1,5 +1,15 @@
 import Dexie from 'dexie'
 
+// 1ページあたりの10件でページネーションする
+const NUM_PER_PAGE: number = 10
+
+export const getMemoPageCount = async (): Promise<number> => {
+  const totalCount = await memos.count()
+  const pageCount = Math.ceil(totalCount / NUM_PER_PAGE)
+  return pageCount > 0 ? pageCount : 1
+}
+
+
 // IndexedDB に保存するデータの型を定義
 export interface MemoRecord {
   datetime: string
@@ -18,4 +28,14 @@ const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
 export const putMemo = async (title: string, text: string): Promise<void> => {
   const datetime = new Date().toISOString()
   await memos.put({ datetime, title, text })
+}
+
+export const getMemos = (page: number): Promise<MemoRecord[]> => {
+  const offset = (page - 1) * NUM_PER_PAGE
+  // memos テーブルからデータを取得する処理
+  return memos.orderBy('datetime')
+              .reverse()
+              .offset(offset)
+              .limit(NUM_PER_PAGE)
+              .toArray()
 }
